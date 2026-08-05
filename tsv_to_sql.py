209487b1f8863c7f999cd6521e802d4007f0592e
Adin Jennings <adinjennings@gmail.com>
2026-08-05 17:25:42 -0400

    tsv to sql now can read sample location into database

--- tsv_to_sql.py
+++ tsv_to_sql.py
@@ -12,42 +12,45 @@ ubw_colonade_gw_data = sqla.Table(
     'ubw_colonade_gw_results',
     metadata_obj,
     sqla.Column('id', sqla.Integer, primary_key=True, autoincrement=True),
-    sqla.Column('Location Name', sqla.String),
-    sqla.Column('Analyte', sqla.String),
-    sqla.Column('Sample Date', sqla.Date),
-    sqla.Column('Result', sqla.Float),
-    sqla.Column('Result Unit', sqla.String),
-    sqla.Column('Method Detection Limit', sqla.Float),
+    sqla.Column('Location Name', sqla.String, sqla.ForeignKey('ubw_colonade_gw_locations.Location_Name'), nullable = False),
+    sqla.Column('Analyte', sqla.String, nullable = False),
+    sqla.Column('Sample Date', sqla.Date, nullable = False),
+    sqla.Column('Result', sqla.Float, nullable = False),
+    sqla.Column('Result Unit', sqla.String, nullable = False),
+    sqla.Column('Method Detection Limit', sqla.Float, nullable = False),
     sqla.Column('Flag', sqla.String),
-    sqla.Column('Detect', sqla.Boolean),
-    sqla.Column('Trace', sqla.Boolean),
-    sqla.Column('Duplicate', sqla.Boolean),
-    sqla.Column('Exclude', sqla.Boolean),
+    sqla.Column('Detect', sqla.Boolean, nullable = False),
+    sqla.Column('Trace', sqla.Boolean, nullable = False),
+    sqla.Column('Duplicate', sqla.Boolean, nullable = False),
+    sqla.Column('Exclude', sqla.Boolean, nullable = False),
     sqla.Column('Chem Group', sqla.String)
 )
 
-ubw_colonade_gw_locations = sqla.Table(
+ubw_colonade_gw_locations_table = sqla.Table(
     'ubw_colonade_gw_locations',
     metadata_obj,
-    sqla.Column('Location Name', sqla.String, primary_key=True),
+    sqla.Column('Location_Name', sqla.String, primary_key=True),
     sqla.Column('X Coordinate', sqla.Float),
     sqla.Column('Y Coordinate', sqla.Float),
     sqla.Column('Longitude', sqla.Float),
     sqla.Column('Latitude', sqla.Float),
     sqla.Column('Layer', sqla.String),
-    sqla.Column('Source_Tail', sqla.CheckConstraint("status IN ('S', 'T') OR status IS NULL")),
+    sqla.Column('Source_Tail', sqla.String, sqla.CheckConstraint("Source_Tail IN ('S', 'T') OR Source_Tail IS NULL")),
     sqla.Column('Saturated Thickness', sqla.Float),
     sqla.Column('Units of ST', sqla.String),
     sqla.Column('Porosity', sqla.Float)
 )
+metadata_obj.create_all(engine)
 
-#with engine.connect() as conn:
-    #for row in df.iloc[:, 2]:
-        #result = conn.execute(
-            #sqla.insert(ubw_colonade_gw_locations),[{"Location Name": row}],)
-        #conn.commit()
+with engine.connect() as conn:
+    for row in df.iloc[:, 2].unique():
+        result = conn.execute(
+            sqla.insert(ubw_colonade_gw_locations_table),
+            [{"Location_Name": row}]
+        )
+        conn.commit()
 
-# stmt = sqla.select(ubw_colonade_gw_locations.c['Location Name'])
-# with engine.connect() as conn:
-    #for row in conn.execute(stmt):
-        #print(row)
+stmt = sqla.select(ubw_colonade_gw_locations_table)
+with engine.connect() as conn:
+    for row in conn.execute(stmt):
+        print(row)
